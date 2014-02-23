fefe6f2cf02d6f84e65ebf0c6b73a8c12e087d8f
Blockhaus2000 <blockhausgames@googlemail.com>
2014-02-23 14:37:48 +0100

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-screen -S "eclipse-bukkit-debugger" -p 0 -X stuff "`printf "stop\r"`"
-
-sleep 2s
-
-screen -A -m -d -S "eclipse-bukkit-debugger" java -ea -jar craftbukkit.jar
-
-gnome-terminal --execute screen -r "eclipse-bukkit-debugger"
