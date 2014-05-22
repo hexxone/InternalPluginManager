b9b02677f8c69d1fcdb05f8bfa2fb87178381753
Blockhaus2000 <blockhausgames@googmail.com>
2014-05-22 18:06:02 +0200

    ? fixed maven build

--- maven-build.sh
+++ maven-build.sh
@@ -8,7 +8,7 @@ case $1 in
     deploy)
 	echo "Starting Maven BUILD and DEPLOY process in 5 seconds! Last possibility to abort!"
 	sleep 5s
-	mvn clean buildnumber:create source:jar javadoc:jar compile replacer:replace package bukkit-tester-maven-plugin:bukkit-test
+	mvn clean buildnumber:create source:jar javadoc:jar compile replacer:replace package bukkit-tester-maven-plugin:bukkit-test deploy
 	;;
     package)
 	echo "Starting Maven BUILD and PACKAGE process in 5 seconds! Last possibility to abort!"
