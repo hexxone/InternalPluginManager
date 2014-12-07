7a5a4f4a99e57d6a00a47b3d06d7fd71a925c141
Blockhaus2000 <blockhausgames@googmail.com>
2014-12-07 17:16:31 +0100

    FIXED ISSUE #198: Renamed packages. * renamed package XXX.util to XXX.base

--- internalpluginmanager-base-api/src/test/java/com/blockhaus2000/ipm/base/ArrayUtilTest.java
+++ internalpluginmanager-base-api/src/test/java/com/blockhaus2000/ipm/base/ArrayUtilTest.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.blockhaus2000.ipm.util;
+package com.blockhaus2000.ipm.base;
 
 import org.junit.Assert;
 import org.junit.Test;
--- internalpluginmanager-base-api/src/test/java/com/blockhaus2000/ipm/base/injection/InjectionManagerTest.java
+++ internalpluginmanager-base-api/src/test/java/com/blockhaus2000/ipm/base/injection/InjectionManagerTest.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.blockhaus2000.ipm.util.injection;
+package com.blockhaus2000.ipm.base.injection;
 
 import org.junit.Assert;
 import org.junit.Test;
