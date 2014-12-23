54ad8b3823c7e80a9a985aff4444122bf9fe9063
fabian <fabian@blockhaus2000.com>
2014-12-23 23:51:34 +0100

    ISSUE #197: Added (dummy) class to fox Jenkins build. 		Please read Javadoc to understand.

---
+++ internalpluginmanager-api/src/main/java/com/blockhaus2000/ipm/api/dummy/DummyClass.java
@@ -0,0 +1,30 @@
+/* This file is part of the InternalPluginManager.
+ *
+ * Copyright (C) 2014 Fabian Damken
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.blockhaus2000.ipm.api.dummy;
+
+/**
+ * Each project has to have at least one class, because the JaCoCo plugin of the
+ * Jenkins CI server searchs for classes in the folder
+ * <code>target/classes/</code>. If no class exists, the folder is empty and an
+ * exception is thrown during the publishing of the JaCoCo test coverage
+ * results.
+ *
+ */
+public class DummyClass {
+    // Nothing to do here. Please read Javadoc for more information.
+}
